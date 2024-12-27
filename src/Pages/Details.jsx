fa97c14603fbe192dab0e1c6d08c0d1fa6693c74
atishBagate <bagateatish@gmail.com>
2024-12-27 18:03:55 +0530

    extra space added

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router-dom';
 function details() {
     const history = useHistory();
     return (
-        <div>This is details page.
+        <div>This is details page. 
              <br/><br/><br/>
             <Button title="Go to Home" onClick={() => { history.push("/") }} />
         </div>
